460958e7c1169e4ac8eb6af07991cb49929bde40
Artem Karpovich <116251264+Morzhig@users.noreply.github.com>
2022-12-04 22:46:11 +0300

    Delete Table_Books directory

--- Table_Books/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.table_books {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.google.gson;
-
-
-    opens com.example.table_books to javafx.fxml;
-    exports com.example.table_books;
-}
