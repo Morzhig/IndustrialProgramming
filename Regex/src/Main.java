0156a3eb768469ecc4817b9d4b411010f97aa5f6
Artem Karpovich <116251264+Morzhig@users.noreply.github.com>
2022-12-18 22:45:05 +0300

    Add files via upload

---
+++ Regex/src/Main.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.regex.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+
+        double res = 0;
+
+        StringTokenizer st = new StringTokenizer(str, "\n ?!,");
+
+        while (st.hasMoreTokens()) {
+            String tmp = st.nextToken();
+
+            if (tmp.matches("[-]?[0-9]+[.][0-9]+[fd]?")) {
+                res += Double.parseDouble(tmp);
+                System.out.println(tmp);
+
+            } else if (tmp.matches("[-]?[0-9][.][0-9]+e[+-][0-9]+")) {
+                res += Double.parseDouble(tmp);
+                System.out.println(tmp);
+
+            } else if(tmp.matches("([-]?[0-9]+[.]?[0-9]*)")) {
+                    res += Double.parseDouble(tmp);
+                    System.out.println(tmp);
+            }
+        }
+
+        System.out.println("The result is: " + res);
+    }
+}
